8e6d8eeb85f65ea7f40e2b621b32df021b076377
Angel <97441137+Soul1227@users.noreply.github.com>
2023-10-10 13:29:15 +0200

    RoutineServiceTest Done
    -All methods have been tested successfully

--- src/main/java/org/arc/rutinabuilder/Services/RoutineService.java
+++ src/main/java/org/arc/rutinabuilder/Services/RoutineService.java
@@ -50,9 +50,10 @@ public class RoutineService {
      *
      * @param routine The routine containing exercises with assigned collections.
      */
-    private void FillExercises(Routine routine) {
+    public void FillExercises(Routine routine) {
         for (Exercise exercice : routine.getExercices()) {
-            exercice = mapNameCollection.get(exercice.getCollection());
+            Exercise exerciceMap = mapNameCollection.get(exercice.getCollection());
+            copyExerciseValues(exercice,exerciceMap);
             exercice.setId(null);
             exercice.setDone(false);
         }
@@ -64,7 +65,7 @@ public class RoutineService {
      * @param routine            The routine containing exercises to be assigned collections.
      * @param listCollectionName A list of collection names to use for assignment.
      */
-    private void AssignCollection(Routine routine, List<String> listCollectionName) {
+    public void AssignCollection(Routine routine, List<String> listCollectionName) {
         int collectionIndex = 0;
         for (Exercise exercise : routine.getExercices()) {
             if (exercise.getCollection() == null || exercise.getCollection().isEmpty()) {
@@ -80,11 +81,17 @@ public class RoutineService {
      *
      * @param routine The input routine for which data is being prepared.
      */
-    private void PreparingList(Routine routine) {
+    public void PreparingList(Routine routine) {
         setListCollectionNameBanned(routine);
         setListCollectionName(exerciceService.getAllCollectionNames().stream().toList());
-        listCollectionName.forEach(listCollectionNameBanned::remove);
-        FillExerciseMap();
+        for (int i = 0; i < listCollectionName.size(); i++) {
+            String name = listCollectionName.get(i);
+            if (listCollectionNameBanned.contains(name)) {
+                listCollectionName.remove(i);
+                i--;
+            }
+        }
+        fillExerciseMap();
     }
 
     /**
@@ -92,7 +99,7 @@ public class RoutineService {
      *
      * @return A HashMap of Exercice objects, where each object represents the last performed exercise for a muscle group guide collection.
      */
-    public HashMap<String, Exercise> FillExerciseMap() {
+    public HashMap<String, Exercise> fillExerciseMap() {
         List<String> guideCollections = exerciceService.getAllCollectionNames().stream().filter(collectionName -> collectionName.contains("_guide")).toList();
         for (String collectionName : guideCollections) {
             Exercise exercice = exerciceService.findLeastPerformedExercise(collectionName);
@@ -103,13 +110,31 @@ public class RoutineService {
         return mapNameCollection;
     }
 
+    /**
+     * Copies values from one exercise to another, updating the target exercise.
+     *
+     * @param targetExercise The exercise to fill with values.
+     * @param sourceExercise The exercise from which to take values.
+     * @return The updated target exercise.
+     */
+    private Exercise copyExerciseValues(Exercise targetExercise, Exercise sourceExercise) {
+        targetExercise.setName(sourceExercise.getName());
+        targetExercise.setRep(sourceExercise.getRep());
+        targetExercise.setSet(sourceExercise.getSet());
+        targetExercise.setTime(sourceExercise.getTime());
+        targetExercise.setDescription(sourceExercise.getDescription());
+        targetExercise.setDate(sourceExercise.getDate());
+        targetExercise.setWeight(sourceExercise.getWeight());
+        return targetExercise;
+    }
+
     public List<String> getListCollectionNameBanned() {
         return listCollectionNameBanned;
     }
 
     public void setListCollectionNameBanned(Routine routine) {
         for (int index = 0; index < routine.getExercices().size(); index++) {
-            if (!listCollectionNameBanned.contains(routine.getExercices().get(index).getCollection()) && routine.getExercices().get(index).getCollection()!=null)
+            if (!listCollectionNameBanned.contains(routine.getExercices().get(index).getCollection()) && routine.getExercices().get(index).getCollection() != null)
                 listCollectionNameBanned.add(routine.getExercices().get(index).getCollection());
         }
     }
@@ -119,6 +144,6 @@ public class RoutineService {
     }
 
     public void setListCollectionName(List<String> listCollectionName) {
-        this.listCollectionName = listCollectionName;
+        this.listCollectionName = new ArrayList<>(listCollectionName);
     }
 }
--- src/test/java/org/arc/rutinabuilder/Services/RoutineServiceTest.java
+++ src/test/java/org/arc/rutinabuilder/Services/RoutineServiceTest.java
@@ -3,59 +3,110 @@ package org.arc.rutinabuilder.Services;
 import org.arc.rutinabuilder.Entity.Exercise;
 import org.arc.rutinabuilder.Entity.Routine;
 import org.junit.jupiter.api.*;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
 import org.springframework.data.mongodb.core.MongoTemplate;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 class RoutineServiceTest {
+    Exercise exerciseTest = new Exercise(5L, "test", 5, 4, null, "test", null, false, 5, "test");
+    Set<String> collectionNames = new HashSet<>();
     ArrayList<String> colectionNames = new ArrayList<>(Arrays.asList("chest", "legs", "shoulders", "bicpes", "triceps", "back", "abs"));
+    MongoTemplate mongoTemplateMock;
 
-    @Mock
-    private Routine routineTest;
+    ExerciceService exerciceServiceMock;
+    ExerciceService exerciceService;
 
-    @Mock
-    private ExerciceService exerciceService;
-
-    @Mock
-    private MongoTemplate mongoTemplate;
-
-    @InjectMocks
-    private RoutineService routineService;
+    RoutineService routineService;
+    RoutineService routineServiceMock;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.initMocks(this); // Inicializar las anotaciones Mockito
+        collectionNames.clear();
+        fillCollectionNames();
+        mongoTemplateMock = mock(MongoTemplate.class);
+
+        exerciceService = new ExerciceService(mongoTemplateMock);
+        exerciceServiceMock = mock(ExerciceService.class);
+
+        routineService = new RoutineService(exerciceServiceMock);
+        routineServiceMock = mock(RoutineService.class);
     }
 
     @AfterEach
     void tearDown() {
     }
 
-    @DisplayName("Create Routine")
+    @DisplayName("Create Routine with 3 collections given")
     @Test
-    void createRoutine() {
-        when(routineService.getListCollectionName()).thenReturn(colectionNames);
+    void createRoutine3CollectionGiven() {
+        when(exerciceServiceMock.getAllCollectionNames()).thenReturn(fillCollectionNames());
+        when(exerciceServiceMock.findLeastPerformedExercise(anyString())).thenReturn(exerciseTest);
         ArrayList<Exercise> exerciseList = new ArrayList<>();
-        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, "legs"));
-        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, "chest"));
-        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, "legs"));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, "legs_guide"));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, "chest_guide"));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, "legs_guide"));
         exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
         exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
         exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
         Routine routine = new Routine(exerciseList);
         Routine routine1 = routineService.CreateRoutine(routine);
-        System.out.println(routine1);
+        routine1.getExercices().forEach(exercise -> System.out.println(exercise.toString()));
     }
 
+    @DisplayName("Create Routine with 3 collections given")
+    @Test
+    void createRoutine3CollectionGiven2() {
+        when(exerciceServiceMock.getAllCollectionNames()).thenReturn(fillCollectionNames());
+        when(exerciceServiceMock.findLeastPerformedExercise(anyString())).thenReturn(exerciseTest);
+        ArrayList<Exercise> exerciseList = new ArrayList<>();
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, "legs_guide"));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, "legs_guide"));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, "legs_guide"));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        Routine routine = new Routine(exerciseList);
+        Routine routine1 = routineService.CreateRoutine(routine);
+        routine1.getExercices().forEach(exercise -> System.out.println(exercise.toString()));
+    }
+
+    @DisplayName("Create Routine with 0 collections given")
+    @Test
+    void createRoutine0CollectionGiven() {
+        when(exerciceServiceMock.getAllCollectionNames()).thenReturn(fillCollectionNames());
+        when(exerciceServiceMock.findLeastPerformedExercise(anyString())).thenReturn(exerciseTest);
+        ArrayList<Exercise> exerciseList = new ArrayList<>();
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        exerciseList.add(new Exercise(null, null, null, null, null, null, null, null, null, null));
+        Routine routine = new Routine(exerciseList);
+        Routine routine1 = routineService.CreateRoutine(routine);
+        routine1.getExercices().forEach(exercise -> System.out.println(exercise.toString()));
+    }
+
+    @DisplayName("Preparing List")
+    @Test
+    void preparingList() {
+
+    }
+
+    @DisplayName("Fill Exercises Map")
     @Test
     void fillExerciseMap() {
+        when(exerciceServiceMock.getAllCollectionNames()).thenReturn(fillCollectionNames());
+        HashMap<String, Exercise> map = routineService.fillExerciseMap();
+        assertEquals(true, map.isEmpty());
     }
 
     @Test
@@ -88,5 +139,15 @@ class RoutineServiceTest {
         System.out.println("Colecciones disponibles" + colectionNames);
         System.out.println("Colecciones no disponibles" + routineService.getListCollectionNameBanned());
         assertTrue(routineService.getListCollectionNameBanned().isEmpty());
+    }
+
+    public Set<String> fillCollectionNames() {
+        collectionNames.add("chest_guide");
+        collectionNames.add("legs_guide");
+        collectionNames.add("shoulders_guide");
+        collectionNames.add("bicpes_guide");
+        collectionNames.add("triceps_guide");
+        collectionNames.add("back_guide");
+        return collectionNames;
     }
 }
